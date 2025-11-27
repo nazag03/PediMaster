b436edcc6984f2676f581c1ca97087a1745ba2c7
Jeremias Brussino <98906318+Assietta@users.noreply.github.com>
2025-11-27 13:05:33 -0300

    Address build warnings and clean caches

--- Back/Infrastructure/Migrations/20251124183912_migraciones.cs
+++ Back/Infrastructure/Migrations/20251124183912_migraciones.cs
@@ -5,7 +5,7 @@
 namespace Infrastructure.Migrations
 {
     /// <inheritdoc />
-    public partial class migraciones : Migration
+    public partial class Migraciones : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
--- Back/WebApi/Program.cs
+++ Back/WebApi/Program.cs
@@ -1,4 +1,3 @@
-
 using Application.Interfaces;
 using Application.Services;
 using Domain;
@@ -10,40 +9,40 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Text;
-using Application.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddScoped<IUserService, UserService>();
 
 // Add services to the container.
-var key = Encoding.UTF8.GetBytes(builder.Configuration.GetSection("Jwt:Key").Value);
+var keyValue = builder.Configuration.GetSection("Jwt:Key").Value
+    ?? throw new InvalidOperationException("JWT signing key configuration is missing.");
+var key = Encoding.UTF8.GetBytes(keyValue);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-
-.AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, jwtOptions =>
-{
-    jwtOptions.TokenValidationParameters = new TokenValidationParameters
+    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, jwtOptions =>
     {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidAudience = builder.Configuration.GetSection("Jwt:Audience").Value,
-        ValidIssuer = builder.Configuration.GetSection("Jwt:Issuer").Value,
-        IssuerSigningKey = new SymmetricSecurityKey(key)
-    };
+        jwtOptions.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidAudience = builder.Configuration.GetSection("Jwt:Audience").Value,
+            ValidIssuer = builder.Configuration.GetSection("Jwt:Issuer").Value,
+            IssuerSigningKey = new SymmetricSecurityKey(key)
+        };
 
-    jwtOptions.MapInboundClaims = false;
-});
-
-builder.Services.AddAuthentication();
+        jwtOptions.MapInboundClaims = false;
+    });
 
 // Register dependencies And Use InMemory Database
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IRestaurantService, RestaurantService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
@@ -62,7 +61,6 @@ builder.Services.AddCors(options =>
         });
 });
 
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -71,7 +69,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 
 app.UseCors("AllowFrontend"); // Para que use el addpolicy
 app.UseHttpsRedirection();
