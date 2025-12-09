f83c3fb45fb9899902c86a2ca18c058d6225ae66
nazag03 <nazagarronr@gmail.com>
2025-12-09 11:16:07 -0300

    Create CRUD for Tags

---
+++ Back/Application/Interfaces/ITagService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.DTOs;
+
+namespace Application.Interfaces
+{
+    public interface ITagService
+    {
+        Task<TagResponseDto> CreateAsync(CreateTagDto dto);
+        Task<IEnumerable<TagResponseDto>> GetAllAsync();
+        Task<TagResponseDto?> GetByIdAsync(int id);
+        Task<IEnumerable<TagResponseDto>> GetByRestaurantAsync(int restaurantId);
+        Task<TagResponseDto?> UpdateAsync(int id, UpdateTagDto dto);
+        Task<bool> DeleteAsync(int id);
+    }
+}
+
---
+++ Back/Application/Services/TagService.cs
@@ -0,0 +1,87 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Services
+{
+    public class TagService : ITagService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TagService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TagResponseDto> CreateAsync(CreateTagDto dto)
+        {
+            var restaurant = await _context.Restaurants.FindAsync(dto.RestaurantId);
+            if (restaurant == null)
+                throw new Exception("Restaurant not found");
+
+            var tag = new Tag
+            {
+                Name = dto.Name,
+                RestaurantId = dto.RestaurantId
+            };
+
+            Microsoft.EntityFrameworkCore.ChangeTracking.EntityEntry<Tag> entityEntry = _context.Tags.Add(tag);
+            await _context.SaveChangesAsync();
+
+            return MapToDto(tag);
+        }
+
+        public async Task<IEnumerable<TagResponseDto>> GetAllAsync()
+        {
+            return await _context.Tags
+                .Select(t => MapToDto(t))
+                .ToListAsync();
+        }
+
+        public async Task<TagResponseDto?> GetByIdAsync(int id)
+        {
+            var tag = await _context.Tags.FindAsync(id);
+            return tag == null ? null : MapToDto(tag);
+        }
+
+        public async Task<IEnumerable<TagResponseDto>> GetByRestaurantAsync(int restaurantId)
+        {
+            return await _context.Tags
+                .Where(t => t.RestaurantId == restaurantId)
+                .Select(t => MapToDto(t))
+                .ToListAsync();
+        }
+
+        public async Task<TagResponseDto?> UpdateAsync(int id, UpdateTagDto dto)
+        {
+            var tag = await _context.Tags.FindAsync(id);
+            if (tag == null) return null;
+
+            tag.Name = dto.Name;
+
+            await _context.SaveChangesAsync();
+            return MapToDto(tag);
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var tag = await _context.Tags.FindAsync(id);
+            if (tag == null) return false;
+
+            _context.Tags.Remove(tag);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        private static TagResponseDto MapToDto(Tag tag)
+        {
+            return new TagResponseDto(
+                tag.TagId,
+                tag.Name,
+                tag.RestaurantId
+            );
+        }
+    }
+}
